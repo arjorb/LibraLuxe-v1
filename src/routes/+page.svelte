663fefa6900d0cfd952cdbce25b4d85ef9b37b81
John U <arjorb@gmail.com>
2024-01-16 10:55:39 +0200

    setting up tailwind css in the project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,1 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class=" text-center text-3xl text-green-400 font-bold my-4">Welcome to SvelteKit</h1>
