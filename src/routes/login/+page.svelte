0bff3733bd71ff2f1f83498ecb7c1fdbbb3c2072
John U <arjorb@gmail.com>
2024-01-17 12:54:31 +0200

    done implementing the desing of the login form and sign up form

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,1 +1,27 @@
-<main>Login</main>
+<main class="flex justify-center mt-64">
+	<div class="w-96">
+		<h1 class="text-4xl font-medium text-gray-600">Log in</h1>
+		<form action="" class="space-y-5 my-7">
+			<label class="flex flex-col text-gray-500">
+				Email
+				<input
+					type="email"
+					placeholder="Enter Email"
+					class="w-full px-2 py-5 border border-gray-200 rounded outline-none"
+				/>
+			</label>
+			<label class="flex flex-col text-gray-500">
+				Password
+				<input
+					type="password"
+					placeholder="Enter Password"
+					class="w-full px-2 py-5 border border-gray-200 rounded outline-none"
+				/>
+			</label>
+			<button class="right-0 px-8 py-2 text-white bg-blue-500 rounded-sm">Log in</button>
+			<p class="text-sm text-gray-400">
+				Don't have an account? <a href="/signup" class="text-blue-500">Sign up</a>
+			</p>
+		</form>
+	</div>
+</main>
--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,1 +1,27 @@
-<main>Sign Up</main>
+<main class="flex justify-center mt-64">
+	<div class="w-96">
+		<h1 class="text-4xl font-medium text-gray-600">Create an Account</h1>
+		<form action="" class="space-y-5 my-7">
+			<label class="flex flex-col text-gray-500">
+				Email
+				<input
+					type="email"
+					placeholder="Enter Email"
+					class="w-full px-2 py-5 border border-gray-200 rounded outline-none"
+				/>
+			</label>
+			<label class="flex flex-col text-gray-500">
+				Password
+				<input
+					type="password"
+					placeholder="Enter Password"
+					class="w-full px-2 py-5 border border-gray-200 rounded outline-none"
+				/>
+			</label>
+			<button class="right-0 px-8 py-2 text-white bg-blue-500 rounded-sm">Sign up</button>
+			<p class="text-sm text-gray-400">
+				Have an account already? <a href="/login" class="text-blue-500">Log in</a>
+			</p>
+		</form>
+	</div>
+</main>
